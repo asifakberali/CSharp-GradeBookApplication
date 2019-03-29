2e4fd2e9158cbbe28a96b16b2b7b3ad482fd4450
ASIF ALI <asif.akberali@outlook.com>
2019-03-28 20:45:59 -0500

    Completed the module

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OnePoint,
+        TwoPoint,
+        ThreePoint,
+        FourPoint,
+        FivePoint,
+        SixPoint
+    }
+}
